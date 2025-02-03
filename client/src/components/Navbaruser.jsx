dd2540dab47e2155987c220ee68ce35375018c46
Abhijith P K <abhijithpkumar123@gmail.com>
2025-02-03 11:21:23 +0530

    navbaruser error fixed

--- client/src/components/Navbaruser.jsx
+++ client/src/components/Navbaruser.jsx
@@ -6,17 +6,17 @@ import { Link, useParams } from "react-router-dom";
 function Navbaruser() {
   const [isProfileOpen, setIsProfileOpen] = useState(false);
   const params = useParams();
+  
   useEffect(() => {
     setIsProfileOpen(false);
   }, [params]);
+
   const [showButton, setShowButton] = useState(false);
 
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900 relative">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <Link to="https://flowbite.com/"
-          className="flex items-center space-x-3 rtl:space-x-reverse"
-        >
+        <Link to="/" className="flex items-center space-x-3">
           <img src={navicon} className="h-8" alt="MyJobGator Logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             MyjobGator
@@ -24,16 +24,12 @@ function Navbaruser() {
         </Link>
         <button
           onClick={() => setShowButton(!showButton)}
-          data-collapse-toggle="navbar-default"
           type="button"
           className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-default"
-          aria-expanded={showButton ? "true" : "false"}
         >
           <span className="sr-only">Open main menu</span>
           <svg
             className="w-5 h-5"
-            aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 17 14"
@@ -47,48 +43,51 @@ function Navbaruser() {
             />
           </svg>
         </button>
+
         {/* Navigation Menu */}
         <div
           className={`${
             showButton ? "block" : "hidden"
-          } w-full md:block md:w-auto absolute md:static right-0 top-0 md:flex`}
+          } w-full md:block md:w-auto md:static`}
           id="navbar-default"
         >
           <ul className="font-medium flex flex-col md:flex-row md:space-x-8 bg-white md:bg-transparent dark:bg-gray-800 md:dark:bg-gray-900 md:ml-auto md:justify-end p-4 md:p-0 border md:border-0 rounded-lg shadow md:shadow-none">
             <li>
-              <Link to="/"
+              <Link
+                to="/"
                 className="block py-2 px-3 text-white bg-orange-700 rounded-sm md:bg-transparent md:text-orange-700 md:p-0 dark:text-white md:dark:text-orange-500"
-                aria-current="page"
               >
                 Home
               </Link>
             </li>
             <li>
-              <a
-                href="#about"
-                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-700 md:p-0 dark:text-white md:dark:hover:text-orange-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              <Link
+                to="/about"
+                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-700 md:p-0 dark:text-white md:dark:hover:text-orange-500"
               >
                 About
-              </a>
-            </li>
-            <li>
-              <a
-                href="#jobs"
-                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-700 md:p-0 dark:text-white md:dark:hover:text-orange-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                jobs
-              </a>
-            </li>
-            <li>
-              <Link to="/login"
-                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-700 md:p-0 dark:text-white md:dark:hover:text-orange-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                login
               </Link>
             </li>
             <li>
-              <Link to="signup"
-                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-700 md:p-0 dark:text-white md:dark:hover:text-orange-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              <Link
+                to="/jobs"
+                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-700 md:p-0 dark:text-white md:dark:hover:text-orange-500"
+              >
+                Jobs
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/login"
+                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-700 md:p-0 dark:text-white md:dark:hover:text-orange-500"
+              >
+                Login
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/signup"
+                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-700 md:p-0 dark:text-white md:dark:hover:text-orange-500"
               >
                 Signup
               </Link>
@@ -97,7 +96,7 @@ function Navbaruser() {
               <div className="relative">
                 <button
                   className="flex items-center space-x-2"
-                  onClick={() => setIsProfileOpen((prv) => !prv)}
+                  onClick={() => setIsProfileOpen((prev) => !prev)}
                 >
                   <img
                     src={profilePic}
